62ebddce2fb06c03c5b6d8d6fa1a0b78dcffae22
Essilfie <bquansah007@gmail.com>
2022-04-07 16:56:54 +0000

    A C program that prints a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return:1
  */
 int main(void)
 {
-putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return(1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
