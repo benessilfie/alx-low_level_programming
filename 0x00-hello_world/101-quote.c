d70da9a6d4a86f6620f3ca3af0bb96ce2dff251c
Essilfie <bquansah007@gmail.com>
2022-04-07 16:55:53 +0000

    a C program that prints a string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return(1);
+}
