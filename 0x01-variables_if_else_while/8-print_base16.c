6cef0cf1e8e2ce3b45f6f35ef3a852e5e8ae1db5
Essilfie <bquansah007@gmail.com>
2022-04-08 23:31:25 +0000

    Task 08

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: A program that prints lowercase alphabet in reverse.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char base16;
+for (base16 = '0'; base16 <= '9'; base16++)
+{
+putchar(base16);
+}
+for (base16 = 'a'; base16 <= 'f'; base16++)
+{
+putchar(base16);
+}
+putchar('\n');
+return (0);
+}
