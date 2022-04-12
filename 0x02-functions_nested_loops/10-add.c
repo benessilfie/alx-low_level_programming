f2e2e1d14c3f86273a04a8805e65f7553bf4d6d2
Essilfie <bquansah007@gmail.com>
2022-04-12 19:42:25 +0000

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - A function that adds two integers and returns the result.
  * @firstNum : First integer to add
- * @secondNum @ Second integer to add
+ * @secondNum : Second integer to add
  * Return: 0
  */
 int add(int firstNum, int secondNum)
