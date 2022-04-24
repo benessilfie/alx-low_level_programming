5f76cc19adf771df0d1001fd7817f2e892c24726
Essilfie <bquansah007@gmail.com>
2022-04-24 15:23:00 +0000

    Task 0 - 07

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,12 @@
 int _strcmp(char *s1, char *s2)
 {
 int i;
-/**
-* We have reached the end of S1 OR we have reached the end of S2
-*/
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+int R;
+i = 0;
+while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 {
-if (s1[i] != s2[i])
-{
-return (s1[i] - s2[i]);
+i++;
 }
-}
-return (0);
+R = s1[i] - s2[i];
+return (R);
 }
