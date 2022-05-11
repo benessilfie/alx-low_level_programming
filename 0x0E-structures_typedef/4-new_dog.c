87c4fc44d08221ce514bc6262d89d54d931835b3
Essilfie <bquansah007@gmail.com>
2022-05-11 16:00:27 +0000

    add task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+int _strlen(char *s);
+char *_strcpy(char *destination, char *src);
 /**
  * *new_dog - New dog struct
  * @name: The dog's name
@@ -10,37 +11,61 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i = 0, j = 0, k;
-dog_t *doge;
-while (name[i] != '\0')
-i++;
-while (owner[j] != '\0')
-j++;
-doge = malloc(sizeof(dog_t));
-if (doge == NULL)
+dog_t *dood_the_dog;
+int name_is_dood = 0, owner_is_benjamin = 0;
+if (name != NULL && owner != NULL)
 {
-free(doge);
+name_is_dood = _strlen(name) + 1;
+owner_is_benjamin = _strlen(owner) + 1;
+dood_the_dog = malloc(sizeof(dog_t));
+if (dood_the_dog == NULL)
+return (NULL);
+dood_the_dog->name = malloc(sizeof(char) * name_is_dood);
+if (dood_the_dog->name == NULL)
+{
+free(dood_the_dog);
 return (NULL);
 }
-doge->name = malloc(i * sizeof(doge->name));
-if (doge->name == NULL)
+dood_the_dog->owner = malloc(sizeof(char) * owner_is_benjamin);
+if (dood_the_dog->owner == NULL)
 {
-free(doge->name);
-free(doge);
+free(dood_the_dog->name);
+free(dood_the_dog);
 return (NULL);
 }
-for (k = 0; k <= i; k++)
-doge->name[k] = name[k];
-doge->age = age;
-doge->owner = malloc(j * sizeof(doge->owner));
-if (doge->owner == NULL)
-{
-free(doge->owner);
-free(doge->name);
-free(doge);
-return (NULL);
+dood_the_dog->name = _strcpy(dood_the_dog->name, name);
+dood_the_dog->owner = _strcpy(dood_the_dog->owner, owner);
+dood_the_dog->age = age;
 }
-for (k = 0; k <= j; k++)
-doge->owner[k] = owner[k];
-return (doge);
+return (dood_the_dog);
+}
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+int c = 0;
+for (; *s != '\0'; s++)
+{
+c++;
+}
+return (c);
+}
+/**
+  * _strcpy - Copy a string
+  * @destination: Destination value
+  * @src: Source value
+  * Return: the pointer to destination
+  */
+char *_strcpy(char *destination, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+destination[i] = src[i];
+}
+destination[i++] = '\0';
+return (destination);
 }
