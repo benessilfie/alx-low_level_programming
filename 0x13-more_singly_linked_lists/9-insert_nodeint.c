814cea0db536fe5b418c3bd465bc57caeb275894
Essilfie <bquansah007@gmail.com>
2022-05-27 01:25:18 +0000

    add more linked list assignment

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,43 @@
 #include "lists.h"
 /**
-* insert_nodeint_at_index -  inserts a new node in a linked list,
-at given position
-* @head: The head of the linked list
-* @idx: the index where the new node should be added
-* @n:  data to insert in the new node
-*
-* Return: The address of the new node, or NULL if it failed.
-*/
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *iter;
-unsigned int count = 1;
-if (head)
-{
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-{
-return (NULL);
-}
-new_node->n = n;
-if (idx > 0)
-{
-iter = *head;
-while (iter)
-{
-if (count == idx)
-{
-new_node->next = iter->next;
-iter->next = new_node;
-return (new_node);
-}
-iter = iter->next;
-count++;
-}
-if (idx > count)
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
+new = malloc(sizeof(listint_t));
+if (!new || !head)
 {
 return (NULL);
 }
+new->n = n;
+new->next = NULL;
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+for (i = 0; temp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+return (new);
 }
 else
 {
-new_node->next = *head;
-*head = new_node;
+temp = temp->next;
 }
-return (new_node);
 }
 return (NULL);
 }
