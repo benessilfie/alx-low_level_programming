659fe8cf630b12e7f37f01ac76121b6467e48aae
Essilfie <bquansah007@gmail.com>
2022-05-27 00:31:42 +0000

    add more linked list assignment

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * sum_listint -  returns the sum of all the data (n)
+  * of a listint_t linked list.
+  * @head: The head of the linked list
+  * 
+  * Return: return 0, if the list is empty
+  */
+int sum_listint(listint_t *head)
+{
+int amount = 0;
+if (head)
+{
+while (head)
+{
+amount += head->n;
+head = head->next;
+}
+}
+return (amount);
+}
