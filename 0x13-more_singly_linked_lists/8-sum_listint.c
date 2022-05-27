c2a285429d489a0e576f737eaf819593ff13e9e1
Essilfie <bquansah007@gmail.com>
2022-05-27 01:19:38 +0000

    add more linked list assignment

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
-* sum_listint -  returns the sum of all the data (n) of a linked list.
-* @head: The head of the linked list.
-* 
-* Return: return 0, if the list is empty.
-*/
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
 int sum_listint(listint_t *head)
 {
 int sum = 0;
