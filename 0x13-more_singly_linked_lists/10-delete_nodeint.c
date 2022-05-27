fc7b55eecc88ac1659280e1383c2e4a650da2e65
Essilfie <bquansah007@gmail.com>
2022-05-27 00:38:30 +0000

    add more linked list assignment

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,6 @@
 #include "lists.h"
 /**
   * delete_nodeint_at_index - deletes the node at index
-  *  index of a listint_t linked list.
   * @head: The head of the linked list
   * @index: the index of the node that should be deleted.
   * 
