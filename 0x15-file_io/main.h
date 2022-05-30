c105c363cedc10cd9ddd9151498c0eb8a148606a
Essilfie <bquansah007@gmail.com>
2022-05-30 20:03:59 +0000

    add main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,10 @@
-#ifndef _FILE_IO_H_
-#define _FILE_IO_H_
-#include <sys/stat.h>
+#ifndef MAIN_H
+#define MAIN_H
 #include <sys/types.h>
-#include <sys/uio.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
-int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_file(const char *src, const char *dest);
-#endif
+#endif /* MAIN_H */
