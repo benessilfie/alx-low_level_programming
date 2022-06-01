d9681b4d05cdb50301688ac74f3b82f61afe67bd
Essilfie <bquansah007@gmail.com>
2022-06-01 12:43:49 +0000

    add 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,12 +4,12 @@
   * @n: The number to find the index
   * @index: The index you want to get.
   *
-  * Return: the value of the bit at index index 
-  * or -1 if an error occured
+  * Return: the value of the bit at index index or -1 if an error occured.
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
 if (index > 63)
 return (-1);
+
 return ((n >> index) & 1);
 }
